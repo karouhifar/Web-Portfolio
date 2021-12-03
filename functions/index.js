0a7e6fd51a33b706daec5f66a844815b778ef3b8
Kamyab Rouhifar <krouhifar@myseneca.ca>
2021-12-03 03:45:39 -0500

    update slide show swiper

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,69 @@
+// ---------------------------------
+// ---    Main serve side        ---
+// ---                           ---
+// ---                           ---
+// ---                           ---
+// ---------------------------------
 const functions = require("firebase-functions");
 const express = require("express");
 const expHBS = require("express-handlebars");
-const cors = require('cors');
+const cors = require("cors");
 const path = require("path");
-const http = require('http');
+const http = require("http");
 const { error } = require("jquery");
 const app = express();
 
-//  const HTTP_PORT = process.env.PORT || 9000;
+const HTTP_PORT = process.env.PORT || 9000;
 
 // --- Show the current active page such as Home, about, contact pages
 let headerData = {
-    homeData: false,
-    aboutData: false,
-    contactData: false
+  homeData: false,
+  aboutData: false,
+  contactData: false,
 };
 
 //--------------------- MiddleWares ---------------------//
 
 // config static file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "../public")));
 // Cross-Origin Resource Sharing
 app.use(cors());
 // View engine setup
-app.engine('hbs', expHBS({ extname: '.hbs'}));
-app.set('view engine', 'hbs');
+app.engine("hbs", expHBS({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // viewed at http://localhost:8080 at index.html
-app.get('/', function(req, res) {
-    // res.status(200).sendFile(path.join(__dirname + '/public/index.html'));
-    headerData.homeData = true;
-    headerData.aboutData = false;
-    headerData.contactData = false;
-    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.status(200).render('index', { layout: false, headerDATA: headerData });
-  
+app.get("/", function (req, res) {
+  // res.status(200).sendFile(path.join(__dirname + '/public/index.html'));
+  headerData.homeData = true;
+  headerData.aboutData = false;
+  headerData.contactData = false;
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  res.status(200).render("index", { layout: false, headerDATA: headerData });
 });
 
-app.get('/contact', function(req, res) {
-    // res.status(200).sendFile(path.join(__dirname + '/public/contact.html'));
-    headerData.homeData = false;
-    headerData.aboutData = false;
-    headerData.contactData = true;
-    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.status(200).render('contact', { layout: false, headerDATA: headerData });
- 
+app.get("/contact", function (req, res) {
+  // res.status(200).sendFile(path.join(__dirname + '/public/contact.html'));
+  headerData.homeData = false;
+  headerData.aboutData = false;
+  headerData.contactData = true;
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  res.status(200).render("contact", { layout: false, headerDATA: headerData });
 });
 
-app.get('/about', function(req, res) {
-    // res.status(200).sendFile(path.join(__dirname + '/public/contact.html'));
-    headerData.contactData = false;
-    headerData.homeData = false;
-    headerData.aboutData = true;
-    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.status(200).render('about', { layout: false, headerDATA: headerData });
-    
+app.get("/about", function (req, res) {
+  // res.status(200).sendFile(path.join(__dirname + '/public/contact.html'));
+  headerData.contactData = false;
+  headerData.homeData = false;
+  headerData.aboutData = true;
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  res.status(200).render("about", { layout: false, headerDATA: headerData });
 });
 app.use((req, res) => {
-    res.status(404).render('error',{ layout: false});
-  });
-
-
-exports.app = functions.https.onRequest(app);
+  res.status(404).render("error", { layout: false });
+});
+// TODO
+// Run server using NodeJS localhost:9000
+app.listen(HTTP_PORT, () => console.log("listen to " + HTTP_PORT));
+// Run server using Firebase severless function
+// exports.app = functions.https.onRequest(app);
